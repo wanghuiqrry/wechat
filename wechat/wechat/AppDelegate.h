f298ccf73ed0f57a35abafac6c151cf6e053e47b
peanut <peanut@peanutdeMacBook-Pro.local>
2015-08-30 11:04:55 +0800

    添加了代码仓库

---
+++ wechat/wechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wechat
+//
+//  Created by peanut on 15/8/30.
+//  Copyright (c) 2015年 peanut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wechat/wechat/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  wechat
+//
+//  Created by peanut on 15/8/30.
+//  Copyright (c) 2015年 peanut. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"代码仓库怎么样");
+}
+
+@end
